307941dcce83543d94c39aa9fbba361be9fa69c2
Martin <48153829+sumos77@users.noreply.github.com>
2022-03-16 20:51:21 +0100

    Added word function

--- hello.py
+++ hello.py
@@ -8,3 +8,16 @@ def add(number1, number2):
 
 
 print(add(number1, number2))
+
+
+word = input()
+
+
+def check_letter(word):
+    word_counter = 0
+    for letter in word:
+        if letter == 'a':
+            word_counter += 1
+    return word_counter
+
+print(f'The letter a appears {check_letter(word)} times in the word {word}')
