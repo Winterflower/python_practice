9aa388ab14816826e068be62fefde6a5b178c651
Martin <48153829+sumos77@users.noreply.github.com>
2022-03-16 20:41:09 +0100

    Added add fuńction

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print('Hello I am a Python file! It\'s nice to meet you!')
+number1 = int(input())
+number2 = int(input())
+
+
+def add(number1, number2):
+    return number1 + number2
+
+
+print(add(number1, number2))
